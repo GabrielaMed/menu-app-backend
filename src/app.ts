b61b0ce722dc2dc70b136fb0c19180b3b352c57d
Gabriela Medeiros <gabrielamedeiros998@gmail.com>
2023-03-16 15:51:44 -0400

    feat: add create product route

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
 import express from 'express';
 import morgan from 'morgan';
-import * as teste from './routes';
+import * as product from './routes/product';
 export const app = express();
 
 app.use(morgan('dev'));
 app.use(express.json());
 
-app.use('/', teste.routes);
+app.use('/api/product', product.routes);
