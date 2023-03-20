a3654af27598e473f225d61b43e260d93e41ab36
Gabriela Medeiros <gabrielamedeiros998@gmail.com>
2023-03-20 16:49:36 -0400

    refactor: change variable name and add new controller

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express';
-import { CreateProductController } from './controllers/product/createProductController';
+import { createProductController } from './controllers/product/createProductController';
+import { createAdditionalController } from './controllers/additional/createAdditionalController';
+import { relatesAdditionalProductController } from './controllers/relatesAdditionalProduct/relatesAdditionalProductController';
 const routes = Router();
 
-const test = new CreateProductController();
-routes.post('/api/product', test.createProduct, test.createAdditionals);
+routes.post(
+  '/api/product',
+  createProductController.createProduct,
+  createAdditionalController.createAdditionals,
+  relatesAdditionalProductController.handle
+);
 
 // routes.post("/api/event/", createEventController.createEvent);
 // routes.get("/api/event/", createEventController.getEvents);
