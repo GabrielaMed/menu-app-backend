1d3da90212b62067eb20df113e6f475c463a9b3f
Gabriela Medeiros <gabrielamedeiros998@gmail.com>
2023-03-22 14:29:57 -0400

    feat: add save image route

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,10 @@ import { Router } from 'express';
 import { createProductController } from './controllers/product/createProductController';
 import { createAdditionalController } from './controllers/additional/createAdditionalController';
 import { relatesAdditionalProductController } from './controllers/relatesAdditionalProduct/relatesAdditionalProductController';
+import multer from 'multer';
+import * as multerConfig from './config/multer';
+import { saveImagesController } from './controllers/product/saveImagesController';
+
 const routes = Router();
 
 routes.post(
@@ -9,6 +13,12 @@ routes.post(
   createProductController.createProduct,
   createAdditionalController.createAdditionals,
   relatesAdditionalProductController.handle
+);
+
+routes.post(
+  '/api/:productId/image',
+  multer(multerConfig).single('file'),
+  saveImagesController.handle
 );
 
 export { routes };
