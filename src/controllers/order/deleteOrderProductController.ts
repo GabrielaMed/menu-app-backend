83d56dda1ccc6c91866fbe16168746f8a5584922
Gabriela Medeiros <gabrielamedeiros998@gmail.com>
2023-05-15 17:48:26 -0400

    after delete return order new info

--- src/controllers/order/deleteOrderProductController.ts
+++ src/controllers/order/deleteOrderProductController.ts
@@ -5,7 +5,7 @@ import { AppError } from '../../middlewares/AppErrors';
 export class DeleteOrderProductController {
   async handle(req: Request, res: Response) {
     try {
-      const { orderProductId } = req.body;
+      const { orderProductId, visitorUuid, companyId } = req.body;
 
       await prisma.order_products.deleteMany({
         where: {
@@ -13,7 +13,50 @@ export class DeleteOrderProductController {
         },
       });
 
-      res.status(204).send('Product deleted!');
+      const orderProducts = await prisma.order.findMany({
+        where: {
+          visitorUuid,
+          companyId,
+        },
+        select: {
+          id: true,
+          Order_products: {
+            select: {
+              id: true,
+              observation: true,
+              quantity: true,
+              Order_additional: {
+                select: {
+                  additional: {
+                    select: {
+                      id: true,
+                      name: true,
+                      price: true,
+                    },
+                  },
+                  quantity: true,
+                },
+              },
+              product: {
+                select: {
+                  id: true,
+                  name: true,
+                  price: true,
+                  Image: {
+                    select: {
+                      fileName: true,
+                    },
+                  },
+                },
+              },
+            },
+          },
+          statusOrder: true,
+          _count: true,
+        },
+      });
+
+      res.status(200).json(orderProducts);
     } catch (error) {
       console.log('EROROO', error);
       if (error instanceof Error) {
